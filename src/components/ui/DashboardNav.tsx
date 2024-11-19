5dc831cb70d7f20451af9fcd27ed34dacd12dd18
aurelienLRY <leroyaurelien11@gmail.com>
2024-11-19 13:56:32 +0100

    Styles Navigation

--- src/components/ui/DashboardNav.tsx
+++ src/components/ui/DashboardNav.tsx
@@ -21,40 +21,52 @@ export const DashboardNav = ({
 }: {
   sessionsWithDetails: ISessionWithDetails[];
 }) => {
+  /*usePathname*/
   const pathname = usePathname();
 
+  /**
+   * isActive
+   * @param {string} path - Le chemin de la route.
+   * @returns {boolean} Si la route est active ou inactive.
+   */
   const isActive = (path: string) => {
     return pathname.toString() === path;
   };
 
+  /*get session pending count*/
   const sessionPendingCount = getSessionByStatus(
     sessionsWithDetails,
     "Pending"
   );
+
+  /*get customer waiting count*/
   const customerWaitingCount = countAllWaitingCustomers(sessionsWithDetails);
 
+  /**
+   * isPathActive
+   * @param {string} path - Le chemin de la route.
+   * @returns {string} La classe CSS active ou inactive.
+   */
+  const isPathActive = (path: string) => {
+    return cn(
+      isActive(path) ? "font-semibold bg-sky-700 dark:bg-sky-900" : "",
+      "py-2 px-3 rounded-md transition-all hover:font-semibold "
+    );
+  };
+
   return (
-    <nav className="flex flex-col md:flex-row items-center gap-1 md:gap-2  bg-sky-950 dark:bg-sky-800 text-white text-xs font-light p-1 rounded-md">
-      <div className="flex  ">
-        <Link
-          href="/dashboard"
-          className={cn(
-            isActive("/dashboard")
-              ? "font-semibold bg-sky-700 dark:bg-sky-900"
-              : "",
-            "p-1 px-2 rounded-md transition-all hover:font-semibold "
-          )}
-        >
+    <nav
+      className="flex flex-col sm:flex-row justify-center items-center gap-1 md:gap-2  bg-sky-950 dark:bg-sky-800 text-white text-xs font-light md:p-1 rounded-md
+    fixed bottom-10 translate-y-1/2 py-4 w-full md:w-auto  md:top-0 md:left-0 md:right-0 md:bottom-auto z-10
+    "
+    >
+      <div className="flex relative  ">
+        <Link href="/dashboard" className={isPathActive("/dashboard")}>
           Dashboard
         </Link>
         <Link
           href="/dashboard/session"
-          className={cn(
-            isActive("/dashboard/session")
-              ? "font-semibold bg-sky-700 dark:bg-sky-900"
-              : "",
-            "p-1 px-2 rounded-md transition-all hover:font-semibold  relative"
-          )}
+          className={isPathActive("/dashboard/session")}
         >
           {sessionsWithDetails && sessionPendingCount > 0 && (
             <span className="absolute -top-1 -right-1 w-4 h-4 bg-orange-600 rounded-full text-white p-1 text-xs flex justify-center items-center opacity-80">
@@ -65,12 +77,7 @@ export const DashboardNav = ({
         </Link>
         <Link
           href="/dashboard/booking"
-          className={cn(
-            isActive("/dashboard/booking")
-              ? "font-semibold bg-sky-700 dark:bg-sky-900"
-              : "",
-            "p-1 px-2 rounded-md transition-all hover:font-semibold  relative"
-          )}
+          className={isPathActive("/dashboard/booking")}
         >
           {sessionsWithDetails && customerWaitingCount > 0 && (
             <span className="absolute -top-1 -right-1 w-4 h-4 bg-orange-600 rounded-full text-white p-1 text-xs flex justify-center items-center opacity-80">
@@ -80,30 +87,18 @@ export const DashboardNav = ({
           Réservations
         </Link>
       </div>
-      <div className="flex ">
+      <div className="flex relative  ">
         <Link
           href="/dashboard/spot"
-          className={cn(
-            isActive("/dashboard/spot")
-              ? "font-semibold bg-sky-700 dark:bg-sky-900"
-              : "",
-            "p-1 px-2 rounded-md transition-all hover:font-semibold "
-          )}
+          className={isPathActive("/dashboard/spot")}
         >
-          {" "}
-          Lieux{" "}
+          Lieux
         </Link>
         <Link
           href="/dashboard/activity"
-          className={cn(
-            isActive("/dashboard/activity")
-              ? "font-semibold bg-sky-700 dark:bg-sky-900"
-              : "",
-            "p-1 px-2 rounded-md transition-all hover:font-semibold "
-          )}
+          className={isPathActive("/dashboard/activity")}
         >
-          {" "}
-          Activités{" "}
+          Activités
         </Link>
       </div>
     </nav>
