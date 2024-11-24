4e639c04f8fe08eaef8bf7ec1da3e11ec5989f8b
aurelienLRY <leroyaurelien11@gmail.com>
2024-11-24 13:00:21 +0100

    + GoogleApi | fixe styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function HomePage() {
   }
 
   return (
-    <div className=" h-screen relative -mt-16">
+    <div className="w-full h-screen relative -mt-16">
       <Image
         src="/img/escalade-scroll.jpg"
         alt="image de fond de l'application"
