ab97f7aa290a2bb81db75300a98bbd958ef4220d
aurelienLRY <leroyaurelien11@gmail.com>
2024-12-05 12:41:11 +0100

    Refactor ActivityForm to Improve State Management
    - Changed the initialization of `requiredEquipment` to use a separate `initialValue` variable for better clarity and maintainability.
    - Updated the `RequiredEquipmentSection` component to accept `initialValue` instead of `requiredEquipment`, ensuring consistent prop usage.
    - Removed unnecessary console log statement to clean up the code.
    
    These changes enhance the readability and structure of the ActivityForm component.

--- src/components/ui/activity.form.tsx
+++ src/components/ui/activity.form.tsx
@@ -80,9 +80,10 @@ interface FormulaOptionProps {
  * @component
  */
 export function ActivityForm({ data, isOpen, onClose }: ActivityFormProps) {
-  const [requiredEquipment, setRequiredEquipment] = useState<string>(
-    data?.required_equipment || ""
-  );
+  const initialValue = data?.required_equipment || "";
+  const [requiredEquipment, setRequiredEquipment] =
+    useState<string>(initialValue);
+
   const methods = useForm<TActivityForm>({
     resolver: yupResolver(activitySchema),
     defaultValues: {
@@ -142,7 +143,6 @@ export function ActivityForm({ data, isOpen, onClose }: ActivityFormProps) {
       ...formData,
       required_equipment: requiredEquipment,
     };
-    console.log(COMPLETE_FORM_DATA);
 
     let result;
     if (data?._id) {
@@ -269,7 +269,7 @@ export function ActivityForm({ data, isOpen, onClose }: ActivityFormProps) {
             <Input name="min_age" type="number" label="Age minimum" />
           </div>
           <RequiredEquipmentSection
-            requiredEquipment={requiredEquipment}
+            initialValue={initialValue}
             setRequiredEquipment={setRequiredEquipment}
           />
 
@@ -308,10 +308,10 @@ function PricingColumn({ title, prefix, disabled }: PricingColumnProps) {
  * Section de l'équipement requis avec configuration externalisée
  */
 function RequiredEquipmentSection({
-  requiredEquipment,
+  initialValue,
   setRequiredEquipment,
 }: {
-  requiredEquipment: string;
+  initialValue: string;
   setRequiredEquipment: (content: string) => void;
 }) {
   return (
@@ -326,7 +326,7 @@ function RequiredEquipmentSection({
         <Editor
           textareaName="required_equipment"
           apiKey={process.env.NEXT_PUBLIC_TINYMCE_API_KEY}
-          initialValue={requiredEquipment}
+          initialValue={initialValue}
           init={{ ...EDITOR_CONFIG }}
           onEditorChange={setRequiredEquipment}
         />
