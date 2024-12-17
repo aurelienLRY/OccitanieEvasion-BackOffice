8eaffda09abb773e0ed87fb9432bfbdf88bb87c8
aurelienLRY <leroyaurelien11@gmail.com>
2024-12-17 18:49:41 +0100

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,31 @@
-export type * from "@/types/Session.type";
-export type * from "@/types/User.type";
-export type * from "@/types/Activity.type";
-export type * from "@/types/Spot.type";
-export type * from "@/types/CustomerSession.type";
-export type * from "@/types/SessionWithDetails.type";
-export type * from "@/types/Callback.action.type";
-export type * from "@/types/EmailTemplate.type";
-export type * from "@/types/Calendar.type";
+export type { ISession } from "@/types/Session.type";
+export type { IUser } from "@/types/User.type";
+export type { IActivity } from "@/types/Activity.type";
+export type { ISpot } from "@/types/Spot.type";
+export type { ICustomerSession } from "@/types/CustomerSession.type";
+export type { ISessionWithDetails } from "@/types/SessionWithDetails.type";
+export type {
+  ICallback,
+  ICallbackForActivities,
+  ICallbackForActivity,
+  ICallbackForCredentials,
+  ICallbackForCustomerSession,
+  ICallbackForCustomerSessions,
+  ICallbackForEmailTemplate,
+  ICallbackForEmailTemplates,
+  ICallbackForSession,
+  ICallbackForSessions,
+  ICallbackForSessionWithDetails,
+  ICallbackForSessionWithDetailsArray,
+  ICallbackForSpot,
+  ICallbackForSpots,
+  ICallbackForUser,
+  ICallbackForEvent,
+} from "@/types/Callback.action.type";
+export type { IEmailTemplate } from "@/types/EmailTemplate.type";
+export type {
+  ICalendarEvent,
+  ICalendarTokenInfo,
+  ICredentials,
+  IEventModel,
+} from "@/types/Calendar.type";
