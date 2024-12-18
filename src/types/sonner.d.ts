bce31746243f26583c21783f0ab359f45b4894ee
aurelienLRY <leroyaurelien11@gmail.com>
2024-12-18 19:11:06 +0100

    Update package-lock.json and remove unused TypeScript definitions
    - Updated several dependencies in package-lock.json, including upgrading 'has-bigints' to version 1.1.0, 'is-shared-array-buffer' to version 1.0.4, and 'is-typed-array' to version 1.1.15 for improved functionality and security.
    - Removed the unused TypeScript definition file for 'sonner', streamlining the codebase and eliminating unnecessary files.
    
    These changes enhance dependency management and maintain a cleaner project structure.

--- src/types/sonner.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "sonner" {
-  export const toast: {
-    success: (message: string) => void;
-    error: (message: string) => void;
-    warning: (message: string) => void;
-    info: (message: string) => void;
-  };
-}
